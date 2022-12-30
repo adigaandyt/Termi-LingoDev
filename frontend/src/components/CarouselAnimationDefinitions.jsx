80043a65856cd459e87c7254e0a79faa32973d0c
סאלח פארס <75793688+saleh550@users.noreply.github.com>
2022-12-31 00:29:15 +0200

    slide right and left

--- frontend/src/components/CarouselAnimationDefinitions.jsx
+++ frontend/src/components/CarouselAnimationDefinitions.jsx
@@ -2,7 +2,7 @@
 
 
 
-import React, {useState} from 'react';
+import React, {useState,useRef} from 'react';
 import ReactDOM from 'react-dom';
 import {TiChevronLeftOutline, TiChevronRightOutline} from 'react-icons/ti';
 import { useSelector } from 'react-redux';
@@ -38,9 +38,39 @@ const Card = ({title,concept}) => {
 const Carousel = ({children}) => {
   const [active, setActive] = useState(2);
   const count = React.Children.count(children);
-  
+  const [position, setPosition] = useState(0);
+  const handleRef = useRef(null);
+  const handleTouchStart = (event) => {
+    event.preventDefault();
+    const initialX = event.touches[0].clientX;
+    const initialPosition = position;
+    const onTouchMove = (event) => {
+      const minPosition = 0;
+      const maxPosition = 200;
+      const newPosition = Math.max(minPosition, Math.min(maxPosition, initialPosition + (event.touches[0].clientX - initialX)));
+      setPosition(newPosition);
+    };
+    const onTouchEnd = (event) => {
+      document.removeEventListener('touchmove', onTouchMove);
+      document.removeEventListener('touchend', onTouchEnd);
+      if (event.changedTouches[0].clientX > initialX) {
+        console.log('Moved to the right');
+        if(active > 0){
+          setActive(i => i - 1)
+        }
+      } else if (event.changedTouches[0].clientX < initialX) {
+        console.log('Moved to the left');
+
+        if(active < count - 1){
+          setActive(i => i + 1)
+        }
+      }
+    };
+    document.addEventListener('touchmove', onTouchMove);
+    document.addEventListener('touchend', onTouchEnd);
+  };
   return (
-    <div className='carousel'>
+    <div ref={handleRef}  onTouchStart={handleTouchStart} className='carousel' >
       {active > 0 && <button className='nav left' onClick={() => setActive(i => i - 1)}><TiChevronLeftOutline/></button>}
       {React.Children.map(children, (child, i) => (
         <div className='card-container' style={{
@@ -48,7 +78,7 @@ const Carousel = ({children}) => {
             '--offset': (active - i) / 3,
             '--direction': Math.sign(active - i),
             '--abs-offset': Math.abs(active - i) / 3,
-            'pointer-events': active === i ? 'auto' : 'none',
+            'pointerEvents': active === i ? 'auto' : 'none',
             'opacity': Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1',
             'display': Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block',
           }}>

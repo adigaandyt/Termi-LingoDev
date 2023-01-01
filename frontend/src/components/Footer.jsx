f68791b9560e347b5bfd4447ab86ec7cbd3beea8
saleh550 <salehf550@gmail.com>
2023-01-01 11:48:26 +0000

    clud

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate,useLocation } from 'react-router-dom';
 import "../styles/Footer.css";
 import {CgGames} from 'react-icons/cg';
 import {AiOutlineHome, AiOutlineSetting} from 'react-icons/ai';
@@ -10,28 +10,28 @@ import {VscQuestion} from 'react-icons/vsc';
 
 export default function Footer(){
     const navigate = useNavigate();
-    
+    const location = useLocation();
     return(
         <nav  dir='ltr'  className="footer1">
             <div className="d-flex justify-content-between"> 
 
-                <div className='mx-1 flex-item text-center'>
+                <div className='mx-1 flex-item text-center' id={location.pathname==='/new/concept'&&'input-clicked'}>
                     <input type= "radio" name="nav-item" id="m-home"/><label onClick={() => navigate('/new/concept')} id="l3"><TbPlaylistAdd/></label>
                 </div>
 
-                <div className=' flex-item'>
-                    <input type= "radio" name="nav-item" id="m-search"/><label onClick={() => console.log('/games')} id="l2"><CgGames/></label>
+                <div className=' flex-item' id={location.pathname==='/games'&&'input-clicked'}>
+                    <input type= "radio" name="nav-item" id="m-search"/><label onClick={() => console.log(location.pathname)} id="l2"><CgGames/></label>
                 </div>
 
-                <div className=' flex-item'>
+                <div className=' flex-item' id={location.pathname==='/'&&'input-clicked'}>
                     <input type= "radio" name="nav-item" id="m-home"/><label onClick={() => navigate('/')} id="l1"><AiOutlineHome/></label>
                 </div>
 
-                <div className=' flex-item'>
+                <div className=' flex-item' id={location.pathname==='/about'&&'input-clicked'}>
                     <input type= "radio" name="nav-item" id="m-home"/><label onClick={() => navigate('/about')} id="l4"><VscQuestion/></label>
                 </div>
 
-                <div className='mx-1 flex-item'>
+                <div className='mx-1 flex-item' id={location.pathname==='/setting'&&'input-clicked'}>
                     <input type= "radio" name="nav-item" id="m-profile"/><label onClick={() => console.log('/setting')} id="l5"><AiOutlineSetting/></label>
                 </div>
 

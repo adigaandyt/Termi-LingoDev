4d6e815b621b0fb53706c146af438cb58bbbcd59
סאלח פארס <75793688+saleh550@users.noreply.github.com>
2023-01-16 21:32:48 +0200

    salehvsc

--- frontend/src/features/categories/categorySlice.js
+++ frontend/src/features/categories/categorySlice.js
@@ -2,10 +2,10 @@ import {createSlice,createAsyncThunk} from '@reduxjs/toolkit'
 import categoryService from './categoryService'
 const initialState={ 
     categories:null,
-    isSuccess:false,
-    isError:false,
-    isLoading:false,
-    message:''
+    isCategorySuccess:false,
+    isCategoryError:false,
+    isCategoryLoading:false,
+    Categorymessage:''
 }
 
 
@@ -24,33 +24,47 @@ export const getCategories=createAsyncThunk(
      }
 
 )
+//get categories names
+export const createNewCategoryByUser=createAsyncThunk(
+    'categories/names',
+     async(thunkAPI)=>{
+        try {
+            return await categoryService.getCategories()
+        } catch (error) {
+            const message=(error.response&&error.response.data&&error.response.data.message)
+            ||error.message
+            ||error.toString()
+            return thunkAPI.rejectWithValue(message)
+        }
+     }
 
+)
 
 export const categorySlice=createSlice({
     name:'category',
     initialState,
     reducers:{
         reset:(state)=>{
-            state.isLoading=false
-            state.isError=false
-            state.isSuccess=false
-            state.message=''
+            state.isCategoryLoading=false
+            state.isCategoryError=false
+            state.isCategorySuccess=false
+            state.Categorymessage=''
         }
     },
     extraReducers:(builder)=>{
        builder
             .addCase(getCategories.pending,(state)=>{
-                state.isLoading=true
+                state.isCategoryLoading=true
             })
             .addCase(getCategories.rejected,(state,action)=>{
-                state.isLoading=false
-                state.isError=true
-                state.message=action.payload
+                state.isCategoryLoading=false
+                state.isCategoryError=true
+                state.Categorymessage=action.payload
                 state.categories=null
             })
             .addCase(getCategories.fulfilled,(state,action)=>{
-                state.isLoading=false
-                state.isSuccess=true
+                state.isCategoryLoading=false
+                state.isCategorySuccess=true
                 state.categories=action.payload
             })
         

c6b16c12616ce86b444e92dae0d99bec4b3c05d4
סאלח פארס <75793688+saleh550@users.noreply.github.com>
2023-01-20 00:22:40 +0200

    saleh vsc

--- frontend/src/components/settings/UnAcceptedConceptsList.jsx
+++ frontend/src/components/settings/UnAcceptedConceptsList.jsx
@@ -5,15 +5,17 @@ function UnAcceptedConceptsList(){
     const {unAcceptedConcepts}=useSelector(state=>state.concept)
 
     return(<>
-    <div className="row">
+    <div className="row w-100">
 
         {unAcceptedConcepts&&unAcceptedConcepts.map((concept,index)=>{
             return(
                 <div className=' col-sm-6 col-md-4 col-lg-3 my-1'>
-                <UnAcceptedConceptCard key={index} concept={concept} index={index} />
+                <UnAcceptedConceptCard key={index} concept={concept} index={index}/>
                 </div>
             )
         })}
+        
+
         {/* <div className=' col-sm-6 col-md-4 col-lg-3 my-1'>
         <UnAcceptedConceptCard/>
         </div> */}

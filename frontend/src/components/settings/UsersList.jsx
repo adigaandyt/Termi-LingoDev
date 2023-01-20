35e95b00c06a3346c9d9109087d8146a04196835
סאלח פארס <75793688+saleh550@users.noreply.github.com>
2023-01-20 16:41:55 +0200

    user comp

--- frontend/src/components/settings/UsersList.jsx
+++ frontend/src/components/settings/UsersList.jsx
@@ -2,7 +2,10 @@ import UserCard from "./UserCard"
 
 function UsersList(){
     return(<>
+    <div className="text-center" style={{"margin":"auto"}}>
+
     <UserCard/>
+    </div>
     </>)
 }
 export default UsersList

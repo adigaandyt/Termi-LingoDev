6554e7dc400f28a4c7e4de596c185344f798983e
saleh550 <salehf550@gmail.com>
2023-01-22 04:20:09 +0200

    cloud

--- frontend/src/components/settings/UserCard.jsx
+++ frontend/src/components/settings/UserCard.jsx
@@ -36,18 +36,18 @@ function UserCard({user}){
                 <div className="stats row">
                     <div>
                         <div className="titleUser">Likes</div>
-                        <FcLike className=' display-6 '/>
+                        <FcLike className=' display-4 '/>
                         <div className="value">2</div>
                     </div>
                     <div>
                         <div className="titleUser">Matches</div>
-                            <CgGames className=' display-6 '/>
+                            <CgGames className=' display-4 '/>
                         <div className="value">{user.gamesPlayed}</div>
                     </div>
                     <div id='icon-card'>
                         <div className="titleUser  mb-0" id="title-add">concepts add</div>
                             {/* <i className="fa fa-group"></i> */}
-                            <TbPlaylistAdd className=' display-6 '/>
+                            <TbPlaylistAdd className=' display-4 '/>
                         <div className="value">{user.added_concepts}</div>
                     </div>
 

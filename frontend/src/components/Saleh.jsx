c861f1b06cd61d61a6577af0f04e3a12fc85089c
סאלח פארס <75793688+saleh550@users.noreply.github.com>
2023-05-04 23:18:58 +0300

    saleh

---
+++ frontend/src/components/Saleh.jsx
@@ -0,0 +1,12 @@
+function Test(){
+    return(<>
+    <div class="card">
+  <img src="your-image.jpg" alt="Card Image"/>
+  <div class="card-content">
+    <h3>Card Title</h3>
+    <p>Card Description</p>
+  </div>
+</div>
+    </>)
+}
+export default Test
